a0fb3a475f3de2eec54d2fcd4e6c4021dff55e99
YUGCHR <47811171+YUGCHR@users.noreply.github.com>
2020-02-03 17:24:54 +0000

    init

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './ProfileInfo.module.css';
+
+const ProfileInfo = () => {
+  return (
+    <div>
+      <div>
+        <img src='../../picture1.png' />
+      </div>
+      <div>
+        ava + desc
+      </div>
+    </div>
+  );
+}
+
+export default ProfileInfo;
