293a45a0a22e6c38ba005d086db066ec4bc8cad4
YUGCHR <47811171+YUGCHR@users.noreply.github.com>
2020-02-12 12:43:36 +0000

    newPostElement = React.createRef

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,8 +5,11 @@ import Post from './Post/Post';
 const MyPosts = (props) => {
 
   let postsElements = props.posts.map((p) => { return <Post message={p.message} likes={p.likeCount} /> });
+
+  let newPostElement = React.createRef();
+
   let addPost = () => {
-    let text = document.getElementById('new-post').value;
+    let text = newPostElement.current.value;
     alert(text)
   };
 
@@ -17,7 +20,7 @@ const MyPosts = (props) => {
       </div>
       <div>
         <div>
-          <textarea id='new-post'></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
           <button onClick={addPost}>Add Post</button>
