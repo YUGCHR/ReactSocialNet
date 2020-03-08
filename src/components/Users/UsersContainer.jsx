5866c1ae2dc1f30a0bb9caa4f69560ce9b1325b5
YUGCHR <47811171+YUGCHR@users.noreply.github.com>
2020-03-08 15:24:06 +0000

    56 two container were splited in one

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,42 @@
+import React from 'react';
 import { connect } from 'react-redux';
+import Axios from 'axios';
 import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC } from '../../redux/users-reducer';
-import UsersAPIComponent from './UsersAPIComponent';
+import Users from './Users';
+
+class UsersContainerAPI extends React.Component {
+
+    constructor(props) { super(props); }
+
+    componentDidMount() {
+        Axios
+            .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(Response => {
+                this.props.setUsers(Response.data.items);
+                this.props.setTotalUsersCount(Response.data.totalCount);
+            });
+    }
+
+    onPageChanges = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        Axios
+            .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(Response => {
+                this.props.setUsers(Response.data.items)
+            });
+    }
+
+    render() {
+        return <Users users={this.props.users}
+            totalCount={this.props.totalCount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            onPageChanges={this.onPageChanges}
+            unfollow={this.props.unfollow}
+            follow={this.props.follow}
+        />
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -32,6 +68,6 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-let UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
+let UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersContainerAPI);
 
 export default UsersContainer;
