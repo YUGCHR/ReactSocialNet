9744bea722ed26d39540e518154264aa3a1f9936
YUGCHR <47811171+YUGCHR@users.noreply.github.com>
2020-08-22 01:00:51 +0100

    72 de/activateEditMode = () => {

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,35 +1,34 @@
 import React from "react";
 import s from "./ProfileStatus.module.css";
-import Preloader from "../../common/preloader/Preloader";
 
 class ProfileStatus extends React.Component {
   state = {
     editMode: false,
   };
 
-  activateEditMode() {
+  activateEditMode = () => {
     //console.log("start of setState", this.state.editMode);
     this.setState({ editMode: true });
     //console.log("end of setState", this.state.editMode);
     //this.state.editMode = true;
     //this.forceUpdate();
-  }
+  };
 
-  deActivateEditMode() {    
-    this.setState({ editMode: false });    
-  }
+  deActivateEditMode = () => {
+    this.setState({ editMode: false });
+  };
 
   render() {
     return (
       <div>
         {!this.state.editMode && (
           <div>
-            <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+            <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
           </div>
         )}
         {this.state.editMode && (
           <div>
-            <input autoFocus={true} onBlur={this.deActivateEditMode.bind(this)} value={this.props.status} />
+            <input autoFocus={true} onBlur={this.deActivateEditMode} value={this.props.status} />
           </div>
         )}
       </div>
@@ -38,3 +37,4 @@ class ProfileStatus extends React.Component {
 }
 
 export default ProfileStatus;
+// this.activateEditMode.bind(this)
