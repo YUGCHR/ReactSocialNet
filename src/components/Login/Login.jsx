662437031c9f72c4365657be65190d9044acebf4
YUGCHR <47811171+YUGCHR@users.noreply.github.com>
2020-09-04 12:54:11 +0100

    74 formatting works

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,9 +3,26 @@ import s from "./Login.module.css";
 
 const Login = (props) => {
   return (
-    <header className={s.header}>
-      <div className={s.loginBlock}>LOGIN</div>
-    </header>
+    <div>
+      <header className={s.header}>
+        <div className={s.loginBlock}>LOGIN</div>
+      </header>
+      <form>
+        <div>
+          <input placeholder={"Login"} />
+        </div>
+        <div>
+          <input placeholder={"Password "} />
+        </div>
+        <div>
+          <input type="checkbox" />
+          Please, remember me there...
+        </div>
+        <div>
+          <button>Login</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
