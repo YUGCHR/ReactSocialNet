e6c588d2fe0127481da3f89e956bd7cfd87c4029
YUGCHR <47811171+YUGCHR@users.noreply.github.com>
2020-09-13 00:09:24 +0100

    83 reselect added

--- src/components/common/preloader/preloader.jsx
+++ src/components/common/preloader/preloader.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import loadingImg from '../../../assets/images/tenor.gif';
 
+// first file name letter is lowercase on GitHub
+
 let Preloader = (props) => {
     // eslint-disable-next-line
     return (
