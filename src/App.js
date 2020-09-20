4d46645991baf13085038f1b277e188b84cf3e1c
YUGCHR <47811171+YUGCHR@users.noreply.github.com>
2020-09-20 23:07:13 +0100

    94 HOC withSuspense added

--- src/App.js
+++ src/App.js
@@ -2,31 +2,31 @@ import React, { Component, lazy, Suspense } from "react";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { Route, withRouter } from "react-router-dom";
-import "./App.css";
-import HeaderContainer from "./components/Header/HeaderContainer";
-import Navbar from "./components/Navbar/Navbar";
-//import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import LoginPage from "./components/Login/Login";
-import Preloader from "./components/common/preloader/Preloader";
-import News from "./components/News/News";
-import Music from "./components/Music/Music";
-import Settings from "./components/Settings/Settings";
 import { initializeApp } from "./redux/app-reducer";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/redux-store";
+import { withSuspense } from "./hoc/withSuspense";
+import "./App.css";
+import HeaderContainer from "./components/Header/HeaderContainer";
+import Navbar from "./components/Navbar/Navbar";
+//import DialogsContainer from "./components/Dialogs/DialogsContainer";
+//import ProfileContainer from "./components/Profile/ProfileContainer";
+//import UsersContainer from "./components/Users/UsersContainer";
+//import LoginPage from "./components/Login/Login";
+import Preloader from "./components/common/preloader/Preloader";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const DialogsContainer = lazy(() => import("./components/Dialogs/DialogsContainer"));
-// <Route path="/dialogs" render={() => <LazyLoadingComponent load={DialogsContainer} />} />
-
+const ProfileContainer = lazy(() => import("./components/Profile/ProfileContainer"));
+const UsersContainer = lazy(() => import("./components/Users/UsersContainer"));
+const LoginPage = lazy(() => import("./components/Login/Login"));
 class App extends Component {
   componentDidMount() {
     this.props.initializeApp(); //thunk
   }
-  //let CallProfile = () => <ProfileContainer />;
-  //let CallUsers = () => <UsersContainer />;
   render() {
     if (!this.props.initialized) {
       return <Preloader />;
@@ -37,24 +37,13 @@ class App extends Component {
         <Navbar />
 
         <div className="app-wrapper-content">
-          <Route
-            path="/dialogs"
-            render={() => {
-              return (
-                <div>
-                  <Suspense fallback={<div>LOADING...</div>}>
-                    <DialogsContainer />
-                  </Suspense>
-                </div>
-              );
-            }}
-          />
-          <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
+          <Route path="/dialogs" render={withSuspense(DialogsContainer)} />
+          <Route path="/profile/:userId?" render={withSuspense(ProfileContainer)} />
           <Route path="/news" render={News} />
           <Route path="/music" render={Music} />
-          <Route path="/users" render={() => <UsersContainer />} />
+          <Route path="/users" render={withSuspense(UsersContainer)} />
           <Route path="/settings" render={Settings} />
-          <Route path="/login" render={() => <LoginPage />} />
+          <Route path="/login" render={withSuspense(LoginPage)} />
         </div>
       </div>
     );
